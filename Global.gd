80cf3a194f59735db1becf44b2c15059f1f23c10
ballachm <ballachm@iu.edu>
2023-11-02 14:17:52 -0400

    done

--- Global.gd
+++ Global.gd
@@ -1,5 +1,13 @@
 extends Node
 
+var coins = 0
+
 func _unhandled_input(event):
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
+
+func add_coin():
+	coins +=1
+	var Coins = get_node_or_null("/root/Game/UI/HUD/Coins")
+	if Coins != null:
+		Coins.text = "Coins: " + str(coins)
--- Player/Player.gd
+++ Player/Player.gd
@@ -54,3 +54,8 @@ func set_animation(anim):
 func die():
 	queue_free()
 
+
+
+func _on_coin_collector_body_entered(body):
+	if body.name == "Coins":
+		body.get_coin(global_position)
